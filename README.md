40937da6f8bbfea3e021517001ef610ffcf3680e
NagisaHere <rw75000777@gmail.com>
2024-07-09 16:16:00 +1000

    working on save function

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Terminal version of a to-do list
 
 Functionalities:
-- practice making a Makefile
+- ~~practice making a Makefile~~
 - Add a to-do item
 - remove a to-do item
 - edit a to-do item
--- main.c
+++ main.c
@@ -11,10 +11,55 @@ void hello(void) {
     printf("Hello World!\n");
 }
 
+// What is #define?
+// Used to declare a constant
+
+// declare max number of items and length of todo tasks
+#define maxItems 100
+#define maxLength 50
+
+
+// What are typedefs?
+// typedefs are used to set alternative names to data types (like unsigned int)
+// an example:
+// typedef short unsigned int USHORT
+// short unsigned int (= USHORT)
+
+typedef struct {
+    char task[maxLength]; // I have no idea what this is
+    int completed; // variable to track completion
+} todoItem;
+
+typedef struct {
+    todoItem items[maxItems];
+    int count;
+} todoList;
+
+// make save to file function
+// I have no idea why astersisks are in the parameters but okay?
+void saveToFile(todoList *list, const char *filename) {
+    FILE *fp = fopen(filename, "w");
+
+    if (fp != NULL) {
+        // test write file
+        fprintf(fp, "Test writing line");
+    } else {
+        printf("Error has occured with opening filePath");
+    }
+    fclose(fp);
+}
+
+// make load function
+
+
+
 int main(void) {
     // initialise basic ncurses window
     
     hello();
+
+    // declare new test todoList
+    // then call the saveToFile function
 
     initscr();
     cbreak();
